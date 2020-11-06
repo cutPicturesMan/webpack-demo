5fca02078363ec3b54152d8b21470c517f2e9d94
张臻 <zhangzhen11@jd.com>
2020-11-06 15:37:37 +0800

    feat: u

--- demo/webpack.config.js
+++ demo/webpack.config.js
@@ -1,3 +1,5 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin'); // 通过 npm 安装
+const webpack = require('webpack'); // 用于访问内置插件
 const path = require('path');
 
 const config = {
@@ -5,7 +7,19 @@ const config = {
   entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
-  }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        include: './src'
+      },
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({template: './src/index.html'})
+  ]
 }
 
 module.exports = config;
